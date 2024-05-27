49b7816ec2c3527ea8576f40531a3905c0780895
edl1ne <dostor19@gmail.com>
2024-05-27 08:43:15 +0300

    3.1 done

---
+++ main.js
@@ -0,0 +1,22 @@
+// Виведення всіх основних типів даних
+let stringType = "string";
+let numberType = 42;
+let booleanType = true;
+let undefinedType;
+let nullType = null;
+let objectType = { name: "Artem", age: 23 };
+let arrayType = [1, 2, 3, 4, 5];
+let functionType = function() { return "I am a function"; };
+let symbolType = Symbol("symbol");
+let bigintType = BigInt(1234567890123456789012345678901234567890n);
+
+console.log("Тип stringType: " + typeof stringType); // string
+console.log("Тип numberType: " + typeof numberType); // number
+console.log("Тип booleanType: " + typeof booleanType); // boolean
+console.log("Тип undefinedType: " + typeof undefinedType); // undefined
+console.log("Тип nullType: " + typeof nullType); // object 
+console.log("Тип objectType: " + typeof objectType); // object
+console.log("Тип arrayType: " + typeof arrayType); // object 
+console.log("Тип functionType: " + typeof functionType); // function
+console.log("Тип symbolType: " + typeof symbolType); // symbol
+console.log("Тип bigintType: " + typeof bigintType); // bigint
