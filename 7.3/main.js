8dc585f026e981e3c9d5b3e3cf0b58d5b954da4b
edl1ne <dostor19@gmail.com>
2024-06-12 19:30:26 +0300

    fix 7.3

--- 7.3/main.js
+++ 7.3/main.js
@@ -2,18 +2,22 @@
 function getNumberGreaterThan100() {
     let number;
     for (let i = 0; i < 10; i++) {
-        number = prompt("Please enter a number greater than 100:", "");
-        if (number === null) {
+        let input = prompt("Please enter a number greater than 100:", "");
+        if (input === null) {
             console.log("User cancelled the prompt.");
             return;
         }
-        number = Number(number);
-        if (number > 100) {
-            console.log(number); 
+        number = Number(input);
+        if (isNaN(number)) {
+            console.log("Invalid input. Please enter a number greater than 100.");
+        } else if (number > 100) {
+            console.log(number);
             return;
+        } else {
+            console.log("The number is not greater than 100. Please try again.");
         }
     }
-    console.log(number); 
+    console.log("Failed to get a number greater than 100 after 10 attempts.");
 }
 
 getNumberGreaterThan100();
